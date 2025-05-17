4ac3808c8081190ba5c2eea891849dabeebc1d03
Eshika Dissanayake <151606601+Eshika2@users.noreply.github.com>
2025-05-17 19:34:59 +0530

    Add files via upload

---
+++ 10_GET_POST.php
@@ -0,0 +1,37 @@
+<?php
+/* --- $_GET & $_POST Superglobals -- */
+
+/*
+  We can pass data through urls and forms using the $_GET and $_POST superglobals.
+*/
+
+if (isset($_POST['submit'])) {
+    // echo '<h3>' . $GET['username'] . '</h3>';
+    echo '<h3>' . $_POST['name'] . '</h3>';
+    // echo $_POST['name'];
+    // echo $_POST['age'];
+} 
+
+
+?>
+
+<!-- Pass data through a link -->
+<a href="<?php echo $_SERVER['PHP_SELF']; ?> ?name=Eshika&age=22">Click</a>
+
+
+<br><br>
+
+<!-- Pass data through a form -->
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+<div>
+  <label>Name: </label>
+  <input type="text" name="name">
+</div>
+<br>
+<div>
+  <label>Age: </label>
+  <input type="text" name="age">
+</div>
+<br>
+  <input type="submit" name="submit" value="Submit">
+</form>
